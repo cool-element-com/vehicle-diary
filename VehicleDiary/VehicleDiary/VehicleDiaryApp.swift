fc411cdee147b859d543da8327a471c7e996e743
boyan yankov <boyan.yankov@igsoft.com>
2024-06-13 08:23:30 -0600

    poc1: add starter project

---
+++ VehicleDiary/VehicleDiary/VehicleDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  VehicleDiaryApp.swift
+//  VehicleDiary
+//
+//  Created by boyan.yankov on 2024-06-13.
+//
+
+import SwiftUI
+
+@main
+struct VehicleDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
