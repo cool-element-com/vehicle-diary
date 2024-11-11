86b8a94fb0ca293e835b38f70b8056fcb4938413
Thinka Boutiter <thinkaboutiter@gmail.com>
2024-11-11 23:15:30 +0200

    1-add guildes for ui optimizations

---
+++ VehicleDiary/VehicleDiary/source/presentation/event/EventRecordTimeMileageView.swift
@@ -0,0 +1,18 @@
+//
+//  EventRecordTimeMileageView.swift
+//  VehicleDiary
+//
+//  Created by boyan.yankov on 2024-11-11.
+//
+
+import SwiftUI
+
+struct EventRecordTimeMileageView: View {
+    var body: some View {
+        /// embody all information about the event
+        /// - Dqte
+        /// - Mileage
+        /// - row should show icon for each `EventTimeMileageView Occurrence`
+        Text("EventRecordTimeMileageView")
+    }
+}
