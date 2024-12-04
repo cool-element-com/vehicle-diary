e9fc9ada89fa594a0fb8ed7398be9dfed268144d
Thinka Boutiter <thinkaboutiter@gmail.com>
2024-12-04 06:18:07 +0200

    10: simplify build setSchema script

--- script/setScheme.sh
+++ script/setScheme.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+set -e
+
+WHITE_ON_BLUE='\033[37;44m'
+NC='\033[0m' # no color
+
+log_command() {
+    echo "${WHITE_ON_BLUE}${1}${NC}"
+}
+
+log_command "pwd"
 pwd
-cd iOS/VehicleDiary
-ls -aril
-scheme_list=$(xcodebuild -list -json | tr -d "\n")
-echo $scheme_list
+# cd iOS/VehicleDiary
+# ls -aril
+# scheme_list=$(xcodebuild -list -json | tr -d "\n")
 # default=$(echo $scheme_list | ruby -e "require 'json'; puts JSON.parse(STDIN.gets)['project']['targets'][0]")
 # echo $default | cat >default
 # echo Using default scheme: $default
