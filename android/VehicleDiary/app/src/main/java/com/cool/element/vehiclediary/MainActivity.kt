5b8b5056747b21c95eb6c4217d2146d28d829eec
boyan yankov <thinkaboutiter@gmail.com>
2025-02-10 09:14:55 +0200

    20: remove incorrect implementations

--- android/VehicleDiary/app/src/main/java/com/cool/element/vehiclediary/MainActivity.kt
+++ android/VehicleDiary/app/src/main/java/com/cool/element/vehiclediary/MainActivity.kt
@@ -4,48 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.cool.element.vehiclediary.presentation.navigation.VehicleDiaryNavHost
 import com.cool.element.vehiclediary.presentation.vehicle.FakeVehiclesListViewModelImpl
-import com.cool.element.vehiclediary.presentation.vehicle.VehicleListView
-import com.cool.element.vehiclediary.ui.theme.VehicleDiaryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-//            val navController = rememberNavController()
-//            VehicleDiaryNavHost(navController = navController)
             VehicleDiaryNavHost(
                 viewModel = FakeVehiclesListViewModelImpl(),
                 navController = rememberNavController()
             )
         }
     }
-}
-
-@Composable
-fun VehicleDiaryAppStart() {
-    VehicleDiaryTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            VehicleListView(
-                viewModel = FakeVehiclesListViewModelImpl(),
-                modifier = Modifier.padding(innerPadding),
-                navController = rememberNavController()
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun VehicleDiaryAppStartPreview() {
-    VehicleDiaryAppStart()
 }
